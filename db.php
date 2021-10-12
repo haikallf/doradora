38632a17be29cdc9c7e900ddc88eeed4854b6c7b
Farhan <ahandnr@gmail.com>
2021-10-12 23:22:21 +0700

    fungsi untuk load,insert,update db

--- db.php
+++ db.php
@@ -3,16 +3,16 @@
 
 // 2. Pengelolaan varian dorayaki (admin)
 // kalo admin, pagenya ada semua fungsi dibawah
-// a.Menambah dorayaki
-function addItem($idItem, $jumlah) {
-
+// a.Menambah varian dorayaki
+function addItem($idItem, $namaItem, $deskripsi,int $harga,int $stok, $gambar,int $available) {
+    $db = new SQLite3("database(1).db");
+    $query = $db->query("INSERT INTO item(idItem, namaItem, deskripsi, harga, stok, gambar, available) VALUES ('$idItem', '$namaItem', '$deskripsi', '$harga', '$stok', '$gambar', '$available');");
 }
+
 // b.Melihat varian dorayaki yang di-filter berdasarkan nama varian
-// ini mending load semua, filternya di array phpnya aja, cuman load yg available
 function loadAllItem() {
     $db = new SQLite3("database(1).db");
-    $query = $db->query("SELECT * FROM item WHERE available = 1;");
-
+    $query = $db->query("SELECT * FROM item;");
     $data = array();
     
     while ($row = $query->fetchArray(SQLITE3_ASSOC)) {
@@ -22,22 +22,70 @@ function loadAllItem() {
     return $data;
 }
 
-$tes = loadAllItem();
-print_r($tes);
-// c.Melihat detail dorayaki
+function loadAllAvailableItem() {
+    $db = new SQLite3("database(1).db");
+    $query = $db->query("SELECT * FROM item WHERE available = 1;");
+    $data = array();
+    
+    while ($row = $query->fetchArray(SQLITE3_ASSOC)) {
+        array_push($data, $row);
+    }
+    $db->close();
+    return $data;
+}
+
+function filterItemByName($nama) {
+    $db = new SQLite3("database(1).db");
+    $query = $db->query("SELECT * FROM item WHERE available = 1 AND namaItem LIKE '%$nama%';");
+    $data = array();
+    
+    while ($row = $query->fetchArray(SQLITE3_ASSOC)) {
+        array_push($data, $row);
+    }
+    $db->close();
+    return $data;
+}
+
+// c.Melihat detail dorayaki = ini dari loadAllItem ambil satu elemen
 
 // d.[BONUS] Mengubah informasi tentang varian dorayaki yang sudah ada.
-
+function editItem($idItem, $columnName, $newValue) {
+    $db = new SQLite3("database(1).db");
+    $query = $db->query("UPDATE item SET '$columnName' = '$newValue' WHERE idItem = '$idItem';");
+}
 // e.Menghapus varian dorayaki yang sudah ada.
+function deleteItem($idItem) {
+    $db = new SQLite3("database(1).db");
+    $query = $db->query("UPDATE item SET available = 0 WHERE idItem = '$idItem';");
+}
 
 // 3. Manajemen stok dorayaki (admin)
 // a. Menambah stok varian dorayaki
+function addStokItemAdmin($idItem,int $value, $username, $tanggal) {
+    $db = new SQLite3("database(1).db");
+    $query = $db->query("UPDATE item SET stok = stok + '$value' WHERE idItem = '$idItem';");
+    // cari nama varian
+    $query2 = $db->query("SELECT namaItem FROM item WHERE idItem = '$idItem';");
+    $fetch2 = $query2->fetchArray(SQLITE3_ASSOC);
+    $namaItem = $fetch2['namaItem'];
 
+    // $query3 = $db->query("INSERT INTO riwayat VALUES ('$username', '$namaItem', '$tanggal', '$value';");
+}
+addStokItemAdmin('id1',0,0,0);
 // b. Mengurangi varian dorayaki
+function reduceStokItemAdmin($idItem,int $value) {
+    $db = new SQLite3("database(1).db");
+    $query = $db->query("UPDATE item SET stok = stok - '$value' WHERE idItem = '$idItem';");
+    // cari nama varian
+    $query2 = $db->query("SELECT namaItem FROM item WHERE idItem = '$idItem';");
+    $fetch2 = $query2->fetchArray(SQLITE3_ASSOC);
+    $namaItem = $fetch2['namaItem'];
+
+    // $query3 = $db->query("INSERT INTO riwayat VALUES ('$username', '$namaItem', '$tanggal', '$value';");
+}
 
 // 4. Melihat Daftar Varian Dorayaki (user)
 // a. sama kaya 2b
-
 // b. sama kaya 2c
 
 // 5. BONUS Riwayat perubahan stok
